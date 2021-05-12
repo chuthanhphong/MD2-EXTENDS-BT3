a8cdeec60facd1cbf7f8e99558cf71c4d83bb5b9
chuthanhphong <81786056+chuthanhphong@users.noreply.github.com>
2021-05-12 15:28:01 +0700

    Bt3

---
+++ main.java
@@ -0,0 +1,13 @@
+package baitap3;
+
+public class main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+        point2D.setX(2.3f);
+        point2D.setY(3.3f);
+        System.out.println(point2D.toString());
+        Point3D point3D = new Point3D();
+        point3D.setZ(3.3f);
+        System.out.println(point3D.toString());
+    }
+}
